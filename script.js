e3dc6122566b8d4bef772b885244648a9433c446
sydorova-nataliya <nsidorova200363@gmail.com>
2022-09-22 07:52:15 +0300

    add append

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ $.ajax({
 
 .done(function(users){
   users.forEach(user => {
-    $('.id').append(`${user.id}`)
+
+    
+  $(`table`).append(`<tr class="table-tr"><td>${user.id}</td><td>${user.name}</td><td>${user.username}</td><td>${user.email}</td><td>${user.phone}</td><td>${user.website}</td></tr>`)
+  //$(`table`).append(`<tr class="table-tr"></tr>`);
+  //  $('.table-tr').append(`<td>${user.id}</td>`)
+  //   //$('.id').append(`<td>${user.id}</td>`)
   });
 })
